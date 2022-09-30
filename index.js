cace5e49a7c00fc7a97adb0f7115256c8cdac132
SK <sarthakkarandikar03@gmail.com>
2022-09-30 17:36:24 +0530

    added mongo connection

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 const express = require('express')
 const path = require('path')
+const mongoose=require('mongoose')
 const app = express()
+
+mongoose.connect('mongodb://localhost:27017/campy', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology:true
+})
+    .then(() => {
+    console.log("Database connected")
+    })
+    .catch(() => {
+        console.log("Database not connected")
+    })
+
+   
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname , 'views'))
