083ce801c6d78e28326db21179a0b5b534eacded
SK <sarthakkarandikar03@gmail.com>
2022-09-30 16:44:02 +0530

    created basic express app

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 const express = require('express')
+const path = require('path')
+const app = express()
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname , 'views'))
+
+app.get('/', (req,res) => {
+    res.render('index')
+})
+app.listen('5500', () => {
+    console.log("App listening on port 5500")
+})
+
