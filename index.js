cbe10c3ec76a372d4e235a8ece15502085aac9bb
SK <sarthakkarandikar03@gmail.com>
2022-10-10 11:10:45 +0530

    added new req

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose')
 const Campground = require('./models/campground')
 
 const app = express()
+app.use(express.urlencoded({ extended :true}))
 
 mongoose.connect('mongodb://localhost:27017/campy', {
     useNewUrlParser: true,
@@ -36,9 +37,20 @@ app.get('/campgrounds', async (req, res) => {
     res.render('campgrounds/index', { campgrounds })
 })
 
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new')
+})
+
 app.get('/campgrounds/:id', async (req, res) => {
     const campground = await Campground.findById(req.params.id)
     res.render('campgrounds/show', {campground})
+})
+
+app.post('/campgrounds', async (req, res) => {
+    console.log(req.body.campground)
+    const campground = new Campground(req.body.campground)
+    await campground.save()
+    res.redirect(`campgrounds/${campground.id}`)
 })
 
 app.listen('5500', () => {
