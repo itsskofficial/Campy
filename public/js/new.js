f8c8f9f8a356c49ba13807414eb1b0e27eb26bac
SK <sarthakkarandikar03@gmail.com>
2022-10-30 06:23:00 +0000

    Oct 30, 2022, 11:53 AM

--- public/js/new.js
+++ public/js/new.js
@@ -2,6 +2,7 @@ var form = document.querySelector('.new-validation')
 console.log(form)
 var inputs = document.querySelectorAll('.input-validation')
 console.log(inputs)
+var 
 
 form.addEventListener('submit',function(event){
     if (!form.checkValidity()){
