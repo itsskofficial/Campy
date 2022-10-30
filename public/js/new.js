8e68beeb3219b50b32dfa2a31a7f31f0f50d4b30
SK <sarthakkarandikar03@gmail.com>
2022-10-30 06:12:00 +0000

    Oct 30, 2022, 11:42 AM

--- public/js/new.js
+++ public/js/new.js
@@ -3,15 +3,15 @@ console.log(form)
 var inputs = document.querySelectorAll('.input-validation')
 console.log(inputs)
 
-        form.addEventListener('submit',function(event){
-            if (!form.checkValidity()){
-                event.preventDefault()
-                event.stopPropagation()
-                Array.from(inputs).forEach(function(input){
-                    input.classList.add('unvalidated')
-                })
-            }
-            Array.from(inputs).forEach(function(input){
-                    input.classList.add('validated')
-                })
-        },false)
+form.addEventListener('submit',function(event){
+    if (!form.checkValidity()){
+        event.preventDefault()
+        event.stopPropagation()
+        Array.from(inputs).forEach(function(input){
+            input.classList.add('unvalidated')
+        })
+    }
+    Array.from(inputs).forEach(function(input){
+            input.classList.add('validated')
+        })
+},false)
