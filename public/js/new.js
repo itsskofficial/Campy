22b73ca39729d08165cae76ffbf3dd3f12166ceb
SK <sarthakkarandikar03@gmail.com>
2022-10-30 06:05:36 +0000

    Oct 30, 2022, 11:35 AM

---
+++ public/js/new.js
@@ -0,0 +1,24 @@
+
+        (function(){
+            'use strict'
+
+            var forms=document.querySelectorAll('.validation')
+            var inputs=document.querySelectorAll('.input-validation')
+
+            Array.from(forms)
+                .forEach(function(form){
+                    form.addEventListener('submit',function(event){
+                        if (!form.checkValidity()){
+                            event.preventDefault()
+                            event.stopPropagation()
+                            Array.from(inputs).forEach(function(input){
+                                input.classList.add('unvalidated')
+                            })
+                        }
+                        Array.from(inputs).forEach(function(input){
+                                input.classList.add('validated')
+                            })
+                    },false)
+                })
+            }
+        )
