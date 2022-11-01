c3c802b4e1c64bade0a805e88e4f005b478e63de
SK <sarthakkarandikar03@gmail.com>
2022-11-01 10:11:24 +0000

    Nov 1, 2022, 3:41 PM

--- index.js
+++ index.js
@@ -27,7 +27,9 @@ db.once("open",() => {
    
 app.engine('ejs',ejsMate)
 app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname , 'views'))
+app.set('views', path.join(__dirname, 'views'))
+
+const validateCampground =>
 
 app.get('/makecampground', async (req, res) => {
     res.send("Making campground here")
