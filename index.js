52ad06ea901e209df0494a3a0d9cd904bc2d1727
SK <sarthakkarandikar03@gmail.com>
2022-11-01 10:12:00 +0000

    Nov 1, 2022, 3:42 PM

--- index.js
+++ index.js
@@ -29,7 +29,22 @@ app.engine('ejs',ejsMate)
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
-const validateCampground =>
+const validateCampground = (req, res, next) => {
+    const campgroundSchema = Joi.object({
+        campground: Joi.object({
+            title: Joi.string().required(),
+            price: Joi.number().required().min(0),
+            description: Joi.string().required(),
+            image: Joi.string().required(),
+            location: Joi.string().required()
+        }).required()
+    })
+    const { error } = campgroundSchema.validate(req.body)
+    if (error) {
+        const msg=error.details.map(el=>el.message).join(',')
+        throw new ExpressError(msg,400)
+    }
+}
 
 app.get('/makecampground', async (req, res) => {
     res.send("Making campground here")
