37a09131e055120d4306117d98f4eeea90825a7d
SK <sarthakkarandikar03@gmail.com>
2022-11-01 03:29:46 +0000

    Nov 1, 2022, 8:59 AM

--- public/js/new.js
+++ public/js/new.js
@@ -14,13 +14,13 @@ form.addEventListener('submit',function(event){
         Array.from(inputs).forEach(function(input){
             if (!input.checkValidity()) {
                 input.classList.add('unvalidated')
-                var el = input.nextElementSibling()
+                var el = input.nextElementSibling
                 el.innerHTML = "Something's wrong"
                 el.style.display="block"
             }
             else {
                 input.classsList.add('validated')
-                var el = input.nextElementSibling()
+                var el = input.nextElementSibling
                 el.innerHTML = "Looks good"
                 el.style.display="block"
             }
