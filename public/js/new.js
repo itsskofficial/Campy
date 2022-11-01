3bbd3a932e5b09b4658fe815c8f4b41b9db2e16a
SK <sarthakkarandikar03@gmail.com>
2022-11-01 03:24:50 +0000

    Nov 1, 2022, 8:54 AM

--- public/js/new.js
+++ public/js/new.js
@@ -21,7 +21,7 @@ form.addEventListener('submit',function(event){
                 input.classsList.add('validated')
                 var el = input.nextElementSibling()
                 el.innerHTML = "Looks good"
-                el.prope
+                el.style.display="block"
             }
         })
     }
