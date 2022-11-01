fb7a3e374b135a38c33786f2f8ebbe910af05a42
SK <sarthakkarandikar03@gmail.com>
2022-11-01 07:57:24 +0000

    Nov 1, 2022, 1:27 PM

--- index.js
+++ index.js
@@ -85,7 +85,7 @@ app.all('*', (req, res, next) => {
 
 app.use((err, req, res, next) => {
     const { statusCode = 500, message ="Something went wrong" } = err
-    res.status(statusCode).render('error')
+    res.status(statusCode).render('error',{err})
 })
 
 app.listen('5500', () => {
