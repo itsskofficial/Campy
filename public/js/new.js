4f268bbba3b852d8699fe5253971f2f396c513ff
SK <sarthakkarandikar03@gmail.com>
2022-11-01 03:25:08 +0000

    Nov 1, 2022, 8:55 AM

--- public/js/new.js
+++ public/js/new.js
@@ -15,8 +15,8 @@ form.addEventListener('submit',function(event){
             if (!input.checkValidity()) {
                 input.classList.add('unvalidated')
                 var el = input.nextElementSibling()
-                
                 el.innerHTML = "Something's wrong"
+                el.style.display="block"
             }
             else {
                 input.classsList.add('validated')
