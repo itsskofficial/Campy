162a998235761219dfedd666dff40dd162163f2b
SK <sarthakkarandikar03@gmail.com>
2022-11-01 04:14:49 +0000

    Nov 1, 2022, 9:44 AM

--- index.js
+++ index.js
@@ -68,10 +68,16 @@ app.post('/campgrounds', async (req, res, next) => {
     }
 })
 
-app.put('/campgrounds/:id', async (req, res) => {
-    const campground = await Campground.findById(req.params.id)
-    await campground.update({...req.body.campground })
-    res.redirect(`/campgrounds/${campground.id}`)
+app.put('/campgrounds/:id', async (req, res, next) => {
+    try {
+        const campground = await Campground.findById(req.params.id)
+        await campground.update({...req.body.campground })
+        res.redirect(`/campgrounds/${campground.id}`)
+    }
+    catch (e) {
+        next(e)
+    }
+    
 })
 
 app.delete('/campgrounds/:id', async(req, res)=> {
