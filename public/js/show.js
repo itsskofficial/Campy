185ddfa95acf39c9e34bd603d22a6ae42042d657
SK <sarthakkarandikar03@gmail.com>
2022-11-09 10:21:32 +0000

    Nov 9, 2022, 3:51 PM

--- public/js/show.js
+++ public/js/show.js
@@ -4,3 +4,5 @@ var textarea = document.getElementById('review-body')
 function insertAfter(referenceNode, newNode) {
     referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
   }
+
+form.addEventListener()
