59cf8d14c1219c487588bd15603bd0641da966d7
SK <sarthakkarandikar03@gmail.com>
2022-11-09 09:55:21 +0000

    Nov 9, 2022, 3:25 PM

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const Campground = require('./models/campground')
 const wrapAsync = require('./utils/wrapasync')
 const ExpressError = require('./utils/expresserror')
 const campgroundSchema = require('./schemas')
-const Review=require('./review')
+const Review=require('./models/review')
 
 const app = express()
 app.use(express.urlencoded({ extended: true }))
