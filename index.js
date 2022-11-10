11c3ae057f4b9e1ece143cb0b089dbaeb314b7b3
SK <sarthakkarandikar03@gmail.com>
2022-11-10 08:14:55 +0000

    Nov 10, 2022, 1:44 PM

--- index.js
+++ index.js
@@ -46,6 +46,7 @@ const validateReview = (req, res, next) => {
     const { error } = reviewSchema.validate(req.body)
     if (error) {
         const msg = error.details.map(el=>el.message).join(',')
+        throw new ExpressError(msg,400)
     }
 }
 app.get('/makecampground', async (req, res) => {
