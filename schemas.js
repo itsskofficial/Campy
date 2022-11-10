68cdee6ea1878ec65b46c7e66e3cafb40ae58037
SK <sarthakkarandikar03@gmail.com>
2022-11-10 08:11:09 +0000

    Nov 10, 2022, 1:41 PM

--- schemas.js
+++ schemas.js
@@ -12,7 +12,7 @@ const campgroundSchema = Joi.object({
 const reviewSchema = Joi.object({
     review: Joi.object({
         rating: Joi.number().required(),
-        body:Joi.
+        body:Joi.string().required()
     })
 })
 
