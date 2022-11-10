51d5068f026ebb80affba128d36cf6838a62b101
SK <sarthakkarandikar03@gmail.com>
2022-11-10 09:16:05 +0000

    Nov 10, 2022, 2:46 PM

--- index.js
+++ index.js
@@ -110,7 +110,7 @@ app.post('/campgrounds/:id/reviews', validateReview,wrapAsync(async (req, res, n
 }))
 
 app.delete('/campgrounds/:campId/reviews/:reviewId', wrapAsync(async (req, res, next) => {
-    
+    await Review.findBy
 }))
 
 app.all('*', (req, res, next) => {
