78f27025ccd83a699db5d121aa81fbd4e8c9e13c
SK <sarthakkarandikar03@gmail.com>
2022-11-10 09:18:06 +0000

    Nov 10, 2022, 2:48 PM

--- index.js
+++ index.js
@@ -110,7 +110,7 @@ app.post('/campgrounds/:id/reviews', validateReview,wrapAsync(async (req, res, n
 }))
 
 app.delete('/campgrounds/:campId/reviews/:reviewId', wrapAsync(async (req, res, next) => {
-    await Campground.findByIdAndUpdate()
+    await Campground.findByIdAndUpdate(req.params)
     await Review.findByIdAndDelete(req.params.reviewId)
 
 
