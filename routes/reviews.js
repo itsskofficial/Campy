97135c43a8e6e682bdb850a75c58e67a8af5daf9
SK <sarthakkarandikar03@gmail.com>
2022-11-22 08:14:15 +0000

    Nov 22, 2022, 1:44 PM

---
+++ routes/reviews.js
@@ -0,0 +1,11 @@
+const validateReview = (req, res, next) => {
+    const { error } = reviewSchema.validate(req.body)
+    if (error) {
+        const msg = error.details.map(el=>el.message).join(',')
+        throw new ExpressError(msg,400)
+    }
+    else{
+        next()
+    }
+}
+
