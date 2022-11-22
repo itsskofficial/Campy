9ec9d7b0bdebc13ad0531f9bffe5cb49bf9e872b
SK <sarthakkarandikar03@gmail.com>
2022-11-22 08:00:28 +0000

    Nov 22, 2022, 1:30 PM

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,2 +1,39 @@
 const express = require('express')
-const router=express.Router()
+const router = express.Router()
+
+app.get('/campgrounds', async (req, res) => {
+    const campgrounds = await Campground.find({})
+    res.render('campgrounds/index', { campgrounds })
+})
+
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new')
+})
+
+app.get('/campgrounds/:id', async (req, res) => {
+    const campground = await Campground.findById(req.params.id).populate('reviews')
+    res.render('campgrounds/show', {campground})
+})
+
+app.get('/campgrounds/:id/edit', async (req, res) => {
+    const campground = await Campground.findById(req.params.id)
+    res.render('campgrounds/edit',{campground})
+})
+
+app.post('/campgrounds',validateCampground, wrapAsync(async (req, res, next) => {
+    const campground = new Campground(req.body.campground)
+    await campground.save()
+    res.redirect(`campgrounds/${campground.id}`)
+}))
+
+app.put('/campgrounds/:id', validateCampground, wrapAsync(async (req, res, next) => {
+        const campground = await Campground.findById(req.params.id)
+        await campground.update({...req.body.campground })
+        res.redirect(`/campgrounds/${campground.id}`)
+}))
+
+.delete('/campgrounds/:id', wrapAsync(async (req, res, next) => {
+        const campground = await Campground.findById(req.params.id)
+        await campground.delete()
+        res.redirect('/campgrounds')
+}))
