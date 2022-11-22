b6365929c1f2f68539024d4ce96212bc809d14c4
SK <sarthakkarandikar03@gmail.com>
2022-11-22 08:55:11 +0000

    Nov 22, 2022, 2:25 PM

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride("_method"))
 app.use(express.static(__dirname + '/public'))
 app.use('/camgrounds', campgrounds)
-app.use('/')
+app.use('/campgrounds/:id/reviews')
 
 const sessionConfig = {
     secret: 'ojaswinithegreat',
