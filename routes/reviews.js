e5778c3b7780c23e7797a88371490a253380a827
SK <sarthakkarandikar03@gmail.com>
2022-11-22 08:15:41 +0000

    Nov 22, 2022, 1:45 PM

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,4 +1,12 @@
-const { Router } = require("express")
+const express = require('express')
+const methodOverride=require('method-override')
+const ejsMate=require('ejs-mate')
+const Campground = require('./models/campground')
+const wrapAsync = require('./utils/wrapasync')
+const ExpressError = require('./utils/expresserror')
+const campgroundSchema = require('./schemas')
+
+const router = express.Router()
 
 const validateReview = (req, res, next) => {
     const { error } = reviewSchema.validate(req.body)
