62af389a4abdf5f317d2b8d54a61ebe99de25842
SK <sarthakkarandikar03@gmail.com>
2022-11-28 08:48:23 +0000

    Nov 28, 2022, 2:18 PM

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ app.set('views', path.join(__dirname, 'views'))
 
 app.get('/fakeUser', async (req, res) => {
     const user = new User({ username: 'Sarthak', email: 'abc@gmail.com' })
-    User.register(user,'notagoodpassword')
+    const newUser.register(user,'notagoodpassword')
 })
 app.get('/makecampground', async (req, res) => {
     res.send("Making campground here")
