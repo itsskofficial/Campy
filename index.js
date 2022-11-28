75b0a739b529d491da28b00dea852becab4d77e5
SK <sarthakkarandikar03@gmail.com>
2022-11-28 08:37:00 +0000

    Nov 28, 2022, 2:07 PM

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const Campground = require('./models/campground')
 const ExpressError = require('./utils/expresserror')
 const session = require('express-session')
 const flash=require('connect-flash')
+const passport = require('passport')
+const passportLocal=require('passport-local')
 
 
 const campgrounds = require('./routes/campgrounds')
