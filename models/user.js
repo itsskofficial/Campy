391b0d50fb1fc35cff07e7b4edce9fbcce9883c6
SK <sarthakkarandikar03@gmail.com>
2022-11-28 08:31:26 +0000

    Nov 28, 2022, 2:01 PM

--- models/user.js
+++ models/user.js
@@ -2,4 +2,8 @@ const mongoose = require('mongoose')
 const express = require('express')
 const passportLocalMongoose = require('passport-local-mongoose')
 
-const Schema= mongoose.Schema()
+const Schema = mongoose.Schema
+
+const userSchema = new Schema({
+    email:
+})
