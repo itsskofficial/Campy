00be5f36424ed44f124b97b2111a82ee189ba0a9
SK <sarthakkarandikar03@gmail.com>
2022-11-28 08:42:47 +0000

    Nov 28, 2022, 2:12 PM

--- index.js
+++ index.js
@@ -44,6 +44,7 @@ app.use((req, res, next) => {
 app.use(passport.initialize())
 app.use(passport.session())
 passport.use(passportLocal(User.authenticate()))
+passport.use(de)
 
 
 mongoose.connect('mongodb://localhost:27017/campy', {
