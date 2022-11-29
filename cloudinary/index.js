613714e9e0cd27704d2f864059fc3a82917ace1a
SK <sarthakkarandikar03@gmail.com>
2022-11-29 11:36:27 +0000

    Nov 29, 2022, 5:06 PM

--- cloudinary/index.js
+++ cloudinary/index.js
@@ -10,7 +10,7 @@ cloudinary.config({
 const storage = new CloudinaryStorage({
     cloudinary,
     params: {
-        folder: 'YelpCamp',
+        folder: 'Campy',
         allowedFormats: ['jpeg', 'png', 'jpg']
     }
 });
